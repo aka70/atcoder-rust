1640bdc108a8dfe579325be4042e01008c02a5ef
aka <akatsukisatoshi@gmail.com>
2022-04-24 16:13:38 +0900

    abc94c

--- AtCoderBeginnersSelection/abc_094_c/src/main.rs
+++ AtCoderBeginnersSelection/abc_094_c/src/main.rs
@@ -1,19 +1,24 @@
-use proconio;
-use regex::Regex;
-
+use proconio::input;
 fn main() {
-    proconio::input! {
-        s: String,
+    input! {
+        n:i32,
+        plans:[(i32,i32,i32);n]
     }
 
-    let ans = if Regex::new(r"^(dream|dreamer|erase|eraser)*$")
-        .unwrap()
-        .is_match(&s)
-    {
-        "YES"
-    } else {
-        "NO"
-    };
+    let mut deer = (0, 0, 0);
+    let mut ans = "Yes";
 
-    println!("{}", ans);
+    for (t, x, y) in plans {
+        let d = (x - deer.1).abs() + (y - deer.2).abs();
+        if d <= (t - deer.0) && ((t - deer.0) - d) % 2 == 0 {
+            deer.0 = t;
+            deer.1 = x;
+            deer.2 = y;
+        } else {
+            ans = "No";
+            break;
+        }
+    }
+
+    println!("{}", ans)
 }
